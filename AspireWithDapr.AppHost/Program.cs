c5f6254df608e6420995437d94fb60c3841f6287
moonolgerd <moonolgerd@gmail.com>
2025-02-20 21:02:40 -0500

    Refactor Dapr folder path and state store configuration
    Updated the Dapr folder path to use a specific ".dapr/bin" directory.
    Changed the `AddDaprStateStore` method to assign its result to a `store` variable and added `WaitFor(cache)` for readiness.
    Modified `AddExecutable` calls for "dapr" and "dashboard" to utilize the new path variable.
    Enhanced the API service project to reference the state store configuration.

--- AspireWithDapr.AppHost/Program.cs
+++ AspireWithDapr.AppHost/Program.cs
@@ -3,28 +3,30 @@ using AspireWithDapr.AppHost;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-var profile = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+var daprFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".dapr", "bin");
 
 var builder = DistributedApplication.CreateBuilder(args);
 
 var cache = builder.AddRedis("cache", "redis/redis-stack", 6379);
 
-builder.AddDaprStateStore("statestore", new DaprComponentOptions
+var store = builder.AddDaprStateStore("statestore", new DaprComponentOptions
 {
     LocalPath = "../statestore.yaml"
-});
+}).WaitFor(cache);
 var pubsub = builder.AddDaprPubSub("pubsub", new DaprComponentOptions
 {
     LocalPath = "../pubsub.yaml"
-});
+}).WaitFor(cache);
 
-builder.AddExecutable("dapr", "placement", $"{profile}\\.dapr\\bin", "-port", "6050");
+builder.AddExecutable("dapr", "placement", daprFolder, "-port", "6050")
+    .WithHttpEndpoint(6050, 6050, isProxied: false);
 
-builder.AddExecutable("dashboard", "dashboard", $"{profile}\\.dapr\\bin", "-port", "9999")
+builder.AddExecutable("dashboard", "dapr", daprFolder, "dashboard", "-p", "9999")
     .WithHttpEndpoint(9999, targetPort: 9999, isProxied: false);
 
 var apiService = builder.AddProject<Projects.AspireWithDapr_ApiService>("apiservice")
     .WithReplicas(3)
+    .WithReference(store)
     .WithDaprSidecar("api");
 
 builder.AddProject<Projects.AspireWithDapr_Web>("webfrontend")
