254aec6c9f618d1a702e2b518c2a857ccd11341d
Oleg Merkulov <moonolgerd@gmail.com>
2025-10-08 19:29:46 -0400

    Aspire 9.5 (#5)
    * Commit upgrade plan
    
    * Update Aspire packages to 9.5.1 versions
    
    ---------
    
    Co-authored-by: Oleg Merkulov <moonolgerd@hotmail.com>

---
+++ AspireWithDapr.ApiService/Mutation.cs
@@ -0,0 +1,17 @@
+﻿using AspireWithDapr.Shared;
+using Dapr.Actors.Client;
+
+namespace AspireWithDapr.ApiService;
+
+public class Mutation
+{
+    public async Task<WeatherForecast> AddWeatherForecastAsync(
+        WeatherForecast forecast,
+        IActorProxyFactory actorProxyFactory)
+    {
+        var weatherActor = actorProxyFactory.CreateActorProxy<IWeatherActor>(
+            new Dapr.Actors.ActorId(forecast.City), nameof(WeatherActor));
+        await weatherActor.AddWeatherForecast(forecast);
+        return forecast;
+    }
+}
